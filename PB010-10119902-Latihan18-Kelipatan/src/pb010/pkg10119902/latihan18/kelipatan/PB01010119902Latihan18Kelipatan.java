be3778ca27448a0b5efe5f54e010974757601d6a
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-31 22:25:37 +0700

    Add files via upload

---
+++ PB010-10119902-Latihan18-Kelipatan/src/pb010/pkg10119902/latihan18/kelipatan/PB01010119902Latihan18Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pb010.pkg10119902.latihan18.kelipatan;
+
+/**
+ * 
+ * @author
+ * NAMA     : Muhammad Nurkholiq
+ * KELAS    : IF-10K
+ * NIM      : 10119902
+ * Deskripsi Program : Program ini berisi program untuk Menghitung Kelipatan
+ * 
+ */
+
+public class PB01010119902Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+        // TODO code application logic here
+        double i = 3.5;
+
+        while (i <= 35) 
+        {
+            System.out.println(i);
+
+            i += 3.5;
+        }
+    }
+}
